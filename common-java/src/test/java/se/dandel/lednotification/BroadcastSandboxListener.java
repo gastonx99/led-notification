357cab0fc8e6f86c119b97fdadf4c7cf9f1f2bf8
Anders Lundberg <dan.anders.lundberg@server.fake>
2015-09-06 16:50:33 +0200

    Two programs talking to each other using MulticastSocket

--- common-java/src/test/java/se/dandel/lednotification/BroadcastSandboxListener.java
+++ common-java/src/test/java/se/dandel/lednotification/BroadcastSandboxListener.java
@@ -1,39 +1,40 @@
 package se.dandel.lednotification;
 
 import java.net.DatagramPacket;
-import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.MulticastSocket;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 
 public class BroadcastSandboxListener {
+    private static final String GROUP_IPADDRESS = "224.6.0.0";
+    private static final int PORT = 5001;
     private final Logger logger = Logger.getLogger(getClass());
+    private MulticastSocket socket;
 
     public static void main(String[] args) {
-        BroadcastSandboxListener listener = new BroadcastSandboxListener();
         try {
+            BroadcastSandboxListener listener = new BroadcastSandboxListener();
             listener.listen();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
+    public BroadcastSandboxListener() throws Exception {
+        socket = new MulticastSocket(PORT);
+        InetAddress group = InetAddress.getByName(GROUP_IPADDRESS);
+        socket.joinGroup(group);
     }
 
     private void listen() throws Exception {
-        DatagramSocket socket = null;
-        try {
-            socket = new DatagramSocket(5001, InetAddress.getByName("127.0.0.1"));
-            byte[] buf = new byte[512];
-            DatagramPacket packet = new DatagramPacket(buf, buf.length);
-            while (true) {
-                logger.info("Waiting for data");
-                socket.receive(packet);
-                String message = new String(buf, "UTF8");
-                logger.info("Data received: " + message);
-            }
-        } finally {
-            IOUtils.closeQuietly(socket);
+        byte[] buf = new byte[512];
+        DatagramPacket packet = new DatagramPacket(buf, buf.length);
+        while (true) {
+            logger.info("Waiting for data");
+            socket.receive(packet);
+            String message = new String(buf, "UTF8");
+            logger.info("Data received: " + message);
         }
     }
 
--- common-java/src/test/java/se/dandel/lednotification/BroadcastSandboxPublisher.java
+++ common-java/src/test/java/se/dandel/lednotification/BroadcastSandboxPublisher.java
@@ -4,36 +4,40 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
 
 public class BroadcastSandboxPublisher {
+    private static final String GROUP_IPADDRESS = "224.6.0.0";
+    private static final int PORT = 5001;
     private final Logger logger = Logger.getLogger(getClass());
+    private DatagramSocket socket;
+    private InetAddress group;
 
     public static void main(String[] args) {
-
-        BroadcastSandboxPublisher publisher = new BroadcastSandboxPublisher();
         try {
-            publisher.send("Howdy pardner");
+            BroadcastSandboxPublisher publisher = new BroadcastSandboxPublisher();
+            while (true) {
+                publisher.send("Howdy pardner " + DateTimeFormat.mediumDateTime().print(DateTime.now()));
+                Thread.sleep(3000);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public BroadcastSandboxPublisher() throws Exception {
+        socket = new DatagramSocket(PORT - 1);
+        group = InetAddress.getByName(GROUP_IPADDRESS);
     }
 
     private void send(String message) throws Exception {
         logger.info("Sending message: " + message);
 
         byte[] buf = message.getBytes("UTF8");
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, group, PORT);
+        socket.send(packet);
 
-        DatagramSocket socket = null;
-        try {
-            socket = new DatagramSocket(5001);
-            InetAddress sendAddress = InetAddress.getByName("127.0.0.1");
-            DatagramPacket packet = new DatagramPacket(buf, buf.length, sendAddress, socket.getLocalPort());
-            socket.send(packet);
-
-        } finally {
-            IOUtils.closeQuietly(socket);
-        }
     }
 }
