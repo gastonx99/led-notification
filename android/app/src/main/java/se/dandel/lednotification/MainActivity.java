0808a2bd432de6269003db17453cf61cf84d888e
Anders Lundberg <dan.anders.lundberg@server.fake>
2017-07-19 20:38:28 +0200

    Added skeleton for an android app

---
+++ android/app/src/main/java/se/dandel/lednotification/MainActivity.java
@@ -0,0 +1,44 @@
+package se.dandel.lednotification;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void callButtonClick(View view) {
+    }
+
+    public void messButtonClick(View view) {
+    }
+
+    public void mailButtonClick(View view) {
+    }
+
+    public void facebookButtonClick(View view) {
+    }
+
+    public void homeButtonClick(View view) {
+    }
+
+    public void pingButtonClick(View view) {
+    }
+
+    public void incomingCallButtonClick(View view) {
+    }
+
+    public void acceptCallButtonClick(View view) {
+    }
+
+    public void missedCallButtonClick(View view) {
+    }
+
+    public void denyCallButtonClick(View view) {
+    }
+}
---
+++ android/app/src/main/java/se/dandel/lednotification/TestClientActivity.java
@@ -0,0 +1,13 @@
+package se.dandel.lednotification;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TestClientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testclient);
+    }
+}
