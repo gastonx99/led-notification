dea64c086243de3205eda29c0382ed0d979c29b6
Anders Lundberg <dan.anders.lundberg@server.fake>
2017-07-20 09:57:58 +0200

    Added gradle environment

--- local-tools/project-setenv.bat
+++ local-tools/project-setenv.bat
@@ -3,9 +3,10 @@
 set JAVA_HOME=\Program\jdk8
 set M2_HOME=\Program\maven3
 set ANT_HOME=\Program\ant1.9
+set GRADLE_HOME=\Program\gradle
 
 set BASH_HOME=\Program\git2
 
-set PATH=%JAVA_HOME%\bin;%M2_HOME%\bin;%ANT_HOME%\bin;%PATH%
+set PATH=%JAVA_HOME%\bin;%M2_HOME%\bin;%ANT_HOME%\bin;%GRADLE_HOME%\bin;%PATH%
 
 cd %CURRENTDIR%\..
